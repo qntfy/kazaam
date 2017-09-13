61d2d687160c1813eee358a4266ec453e91a9f00
John Richter <johnrichter@users.noreply.github.com>
2017-09-13 12:30:14 -0400

    Add VSCode debug binary to gitignores. (#57)
    Fix UUID unit test raw JSON specs and input.
    Fix the functionality of two tests.
    Add unit tests to ensure all UUID transforms are processed.
    Fix bug which only processed the first UUID transform.

--- transform/uuid_test.go
+++ transform/uuid_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestUUIDV4(t *testing.T) {
 	spec := `{"a.uuid":{"version": 4}}`
-	jsonIn := `{"a":{"author":"jason","id":2323223}`
+	jsonIn := `{"a":{"author":"jason","id":2323223}}`
 
 	cfg := getConfig(spec, false)
 	kazaamOut, _ := getTransformTestWrapper(UUID, cfg, jsonIn)
@@ -24,7 +24,7 @@ func TestUUIDV4(t *testing.T) {
 
 func TestUUIDVersionError(t *testing.T) {
 	spec := `{"a.uuid":{"version": 6}}`
-	jsonIn := `{"a":{"author":"jason","id":2323223}`
+	jsonIn := `{"a":{"author":"jason","id":2323223}}`
 
 	cfg := getConfig(spec, false)
 	kazaamOut, _ := getTransformTestWrapper(UUID, cfg, jsonIn)
@@ -38,8 +38,8 @@ func TestUUIDVersionError(t *testing.T) {
 }
 
 func TestUUIDNoVersionError(t *testing.T) {
-	spec := `{"a.uuid":{}`
-	jsonIn := `{"a":{"author":"jason","id":2323223}`
+	spec := `{"a.uuid":{}}`
+	jsonIn := `{"a":{"author":"jason","id":2323223}}`
 
 	cfg := getConfig(spec, false)
 	kazaamOut, _ := getTransformTestWrapper(UUID, cfg, jsonIn)
@@ -54,7 +54,7 @@ func TestUUIDNoVersionError(t *testing.T) {
 
 func TestUUIDInValidSpec(t *testing.T) {
 	spec := `{}`
-	jsonIn := `{"a":{"author":"jason","id":2323223}`
+	jsonIn := `{"a":{"author":"jason","id":2323223}}`
 
 	cfg := getConfig(spec, false)
 	kazaamOut, _ := getTransformTestWrapper(UUID, cfg, jsonIn)
@@ -68,7 +68,12 @@ func TestUUIDInValidSpec(t *testing.T) {
 }
 
 func TestUUIDV3WithoutNamespace(t *testing.T) {
-	spec := `{"a.uuid": {"version": 3, "names": [{"path": "a.id", "default": "test"}, {"path": "a.author", "default": "test"}]}}`
+	spec := `{
+		"a.uuid": {
+			"version": 3, 
+			"names": [{"path": "a.id", "default": "test"}, {"path": "a.author", "default": "test"}]
+		}
+	}`
 	jsonIn := `{"a":{"id":2323223}}`
 
 	cfg := getConfig(spec, false)
@@ -82,9 +87,12 @@ func TestUUIDV3WithoutNamespace(t *testing.T) {
 }
 
 func TestUUIDV3InvalidNamespace(t *testing.T) {
-	spec := `{"a.uuid": {"version": 3, "namespace": "test", "names": [{"path": "a.id", "default": "test"},
-	{"path": "a.author", "default": "test"}]}}`
-
+	spec := `{
+		"a.uuid": {
+			"version": 3, "namespace": "test", 
+			"names": [{"path": "a.id", "default": "test"}, {"path": "a.author", "default": "test"}]
+		}
+	}`
 	jsonIn := `{"a":{"id":2323223}}`
 
 	cfg := getConfig(spec, false)
@@ -97,8 +105,12 @@ func TestUUIDV3InvalidNamespace(t *testing.T) {
 }
 
 func TestUUIDV3URLNameSpace(t *testing.T) {
-	spec := `{"a.uuid": {"version": 3, "namespace": "URL", "names": [{"path": "a.id", "default": "test"},
-	{"path": "a.author", "default": "test"}]}}`
+	spec := `{
+		"a.uuid": {
+			"version": 3, "namespace": "URL", 
+			"names": [{"path": "a.id", "default": "test"}, {"path": "a.author", "default": "test"}]
+		}
+	}`
 	jsonIn := `{"a":{"author":"jason","id":2323223}}`
 	jsonOut := `{"a":{"author":"jason","id":2323223,"uuid":"06af7528-f22c-3716-86e0-579192ed244a"}}`
 
@@ -129,8 +141,12 @@ func TestUUIDV3URLNameSpace(t *testing.T) {
 }
 
 func TestUUIDV3DNSNameSpace(t *testing.T) {
-	spec := `{"a.uuid": {"version": 3, "namespace": "DNS", "names": [{"path": "a.id", "default": "test"},
-	{"path": "a.author", "default": "test"}]}}`
+	spec := `{
+		"a.uuid": {
+			"version": 3, "namespace": "DNS", 
+			"names": [{"path": "a.id", "default": "test"}, {"path": "a.author", "default": "test"}]
+		}
+	}`
 	jsonIn := `{"a":{"author":"jason","id":2323223}}`
 	jsonOut := `{"a":{"author":"jason","id":2323223,"uuid":"83e9b77c-641d-331f-961d-bac57a61e534"}}`
 
@@ -161,8 +177,12 @@ func TestUUIDV3DNSNameSpace(t *testing.T) {
 }
 
 func TestUUIDV3OIDNameSpace(t *testing.T) {
-	spec := `{"a.uuid": {"version": 3, "namespace": "OID", "names": [{"path": "a.id", "default": "test"},
-	{"path": "a.author", "default": "test"}]}}`
+	spec := `{
+		"a.uuid": {
+			"version": 3, "namespace": "OID", 
+			"names": [{"path": "a.id", "default": "test"}, {"path": "a.author", "default": "test"}]
+		}
+	}`
 	jsonIn := `{"a":{"author":"jason","id":2323223}}`
 	jsonOut := `{"a":{"author":"jason","id":2323223,"uuid":"d26cc082-0ba8-38a7-a738-983f3830f0cf"}}`
 
@@ -193,8 +213,12 @@ func TestUUIDV3OIDNameSpace(t *testing.T) {
 }
 
 func TestUUIDV3X500NameSpace(t *testing.T) {
-	spec := `{"a.uuid": {"version": 3, "namespace": "X500", "names": [{"path": "a.id", "default": "test"},
-	{"path": "a.author", "default": "test"}]}}`
+	spec := `{
+		"a.uuid": {
+			"version": 3, "namespace": "X500", 
+			"names": [{"path": "a.id", "default": "test"}, {"path": "a.author", "default": "test"}]
+		}
+	}`
 	jsonIn := `{"a":{"author":"jason","id":2323223}}`
 	jsonOut := `{"a":{"author":"jason","id":2323223,"uuid":"438297ee-7562-336d-a913-7e7745455e80"}}`
 
@@ -225,8 +249,12 @@ func TestUUIDV3X500NameSpace(t *testing.T) {
 }
 
 func TestUUIDWithCustomNameSpace(t *testing.T) {
-	spec := `{"a.uuid": {"version": 3, "namespace": "04536ac7-c030-4f10-811b-451bcc4c8ef5", "names": [{"path": "a.id", "default": "test"},
-	{"path": "a.author", "default": "test"}]}}`
+	spec := `{
+		"a.uuid": {
+			"version": 3, "namespace": "04536ac7-c030-4f10-811b-451bcc4c8ef5", 
+			"names": [{"path": "a.id", "default": "test"}, {"path": "a.author", "default": "test"}]
+		}
+	}`
 	jsonIn := `{"a":{"author":"jason","id":2323223}}`
 	jsonOut := `{"a":{"author":"jason","id":2323223,"uuid":"82966ef3-a31d-379a-a266-d8f323901397"}}`
 
@@ -257,8 +285,12 @@ func TestUUIDWithCustomNameSpace(t *testing.T) {
 }
 
 func TestUUIDV3UsingDefaultField(t *testing.T) {
-	spec := `{"a.uuid":{"version": 3, "namespace": "URL", "names": [{"path": "a.id", "default": "test"},
-	{"path": "a.author", "default": "test"}]}}`
+	spec := `{
+		"a.uuid": {
+			"version": 3, "namespace": "URL", 
+			"names": [{"path": "a.id", "default": "test"}, {"path": "a.author", "default": "test"}]
+		}
+	}`
 	jsonIn := `{"a":{"id":2323223}}`
 	jsonOut := `{"a":{"id":2323223,"uuid":"a6cb7732-ccc1-3725-a9dc-040e73e0889b"}}`
 
@@ -289,8 +321,12 @@ func TestUUIDV3UsingDefaultField(t *testing.T) {
 }
 
 func TestUUIDBadNameSpace(t *testing.T) {
-	spec := `{"a.uuid": {"version": 3, "namespace": "not a uuid", "names": [{"path": "a.id", "default": "test"},
-	{"path": "a.author", "default": "test"}]}}`
+	spec := `{
+		"a.uuid": {
+			"version": 3, "namespace": "not a uuid", 
+			"names": [{"path": "a.id", "default": "test"}, {"path": "a.author", "default": "test"}]
+		}
+	}`
 	jsonIn := `{"a":{"id":2323223}}`
 
 	cfg := getConfig(spec, false)
@@ -305,8 +341,12 @@ func TestUUIDBadNameSpace(t *testing.T) {
 }
 
 func TestUUIDV5(t *testing.T) {
-	spec := `{"a.uuid": {"version": 5, "namespace": "URL", "names": [{"path": "a.id", "default": "test"},
-	{"path": "a.author", "default": "test"}]}}`
+	spec := `{
+		"a.uuid": {
+			"version": 5, "namespace": "URL", 
+			"names": [{"path": "a.id", "default": "test"}, {"path": "a.author", "default": "test"}]
+		}
+	}`
 	jsonIn := `{"a":{"author":"jason","id":2323223}}`
 	jsonOut := `{"a":{"author":"jason","id":2323223,"uuid":"388607bf-b5c1-5f55-b10a-6afbbb91e18e"}}`
 
@@ -337,8 +377,15 @@ func TestUUIDV5(t *testing.T) {
 }
 
 func TestUUIDV5UsingDefaultField(t *testing.T) {
-	spec := `{"a.uuid": {"version": 5, "namespace": "URL", "names": [{"path": "a.id", "default": "test"},
-	{"path": "a.author", "default": "go lang rules!"}]}}`
+	spec := `{
+		"a.uuid": {
+			"version": 5, "namespace": "URL", 
+			"names": [
+				{"path": "a.id", "default": "test"}, 
+				{"path": "a.author", "default": "go lang rules!"}
+			]
+		}
+	}`
 	jsonIn := `{"a":{"id":2323223}}`
 	jsonOut := `{"a":{"id":2323223,"uuid":"fb26c0d3-3cd2-514f-aced-2ecd901a1196"}}`
 
@@ -368,9 +415,8 @@ func TestUUIDV5UsingDefaultField(t *testing.T) {
 	}
 }
 
-func TestUUIDV5NoNames(t *testing.T) {
-	spec := `{"a.uuid": {"version": 5, "namespace": "URL", "names": []},
-	{"path": "a.author", "default": "go lang rules!"}]}}`
+func TestUUIDV5NoNamespace(t *testing.T) {
+	spec := `{"a.uuid": {"version": 5, "names": []}}`
 	jsonIn := `{"a":{"id":2323223}}`
 
 	cfg := getConfig(spec, false)
@@ -380,12 +426,10 @@ func TestUUIDV5NoNames(t *testing.T) {
 		t.Error(err)
 		t.FailNow()
 	}
-
 }
 
-func TestUUIDV5NoName(t *testing.T) {
-	spec := `{"a.uuid": {"version": 5, "namespace": "URL"},
-	{"path": "a.author", "default": "go lang rules!"}]}}`
+func TestUUIDV5NoNames(t *testing.T) {
+	spec := `{"a.uuid": {"version": 5, "namespace": "URL"}}`
 	jsonIn := `{"a":{"id":2323223}}`
 
 	cfg := getConfig(spec, false)
@@ -395,14 +439,24 @@ func TestUUIDV5NoName(t *testing.T) {
 		t.Error(err)
 		t.FailNow()
 	}
-
 }
 
 func TestUUIDV5ArrayIndex(t *testing.T) {
-	spec := `{"a.uuid": {"version": 5, "namespace": "URL", "names": [{"path": "a.tags[0]", "default": "test"},
-	{"path": "a.author", "default": "go lang rules!"}]}}`
+	spec := `{
+		"a.uuid": {
+			"version": 5, "namespace": "URL", 
+			"names": [
+				{"path": "a.tags[0]", "default": "test"}, 
+				{"path": "a.author", "default": "go lang rules!"}
+			]
+		}
+	}`
 	jsonIn := `{"a":{"id":2323223, "tags": ["tag1", "tag2"]}}`
-	jsonOut := `{"a":{"id":2323223, "tags": ["tag1", "tag2"],"uuid":"5ae718bd-0add-5bab-a155-340433391b8c"}}`
+	jsonOut := `{
+		"a": {
+			"id": 2323223, "tags": ["tag1", "tag2"], "uuid": "5ae718bd-0add-5bab-a155-340433391b8c"
+		}
+	}`
 
 	cfg := getConfig(spec, false)
 	kazaamOut, err := getTransformTestWrapper(UUID, cfg, jsonIn)
@@ -426,6 +480,23 @@ func TestUUIDV5ArrayIndex(t *testing.T) {
 	if err != nil {
 		t.Error("transformed data didn't contain valid UUID")
 		t.Error(err)
+		t.FailNow()
+	}
+}
+
+func TestUUIDWithMultipleSpecs(t *testing.T) {
+	spec := `{"a.uuid": {"version": 4}, "fl.uuid": {"version": 4}}`
+	jsonIn := `{"a": {"author": "jason", "id": 2323223}}`
+
+	cfg := getConfig(spec, false)
+	kazaamOut, _ := getTransformTestWrapper(UUID, cfg, jsonIn)
+	aOut, _, _, _ := jsonparser.Get([]byte(kazaamOut), "a", "uuid")
+	flOut, _, _, _ := jsonparser.Get([]byte(kazaamOut), "fl", "uuid")
+	_, aErr := uuid.FromString(string(aOut))
+	_, flErr := uuid.FromString(string(flOut))
+
+	if aErr != nil || flErr != nil {
+		t.Error("transformed data didn't contain valid UUID")
 		t.FailNow()
 	}
 }
