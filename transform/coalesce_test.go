27cb9c459bb697a1b5e763e93bc390349f084a50
Ubuntu <josh@qntfy.com>
2018-01-09 19:09:10 +0000

    Add `ignore` array support in Coalesce()

--- transform/coalesce_test.go
+++ transform/coalesce_test.go
@@ -61,3 +61,20 @@ func TestCoalesceWithNotFound(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+func TestCoalesceWithIgnore(t *testing.T) {
+	jsonInput := `{"rating":{"example":{"value":3},"primary":{"value":3},"foo":5,"bar":""}}`
+	spec := `{"foo": ["rating.foo", "rating.primary"], "bar": ["rating.bar", "rating.example.value"], "ignore": [5, ""]}`
+	jsonOut := `{"rating":{"example":{"value":3},"primary":{"value":3},"foo":5,"bar":""},"foo":{"value":3},"bar":3}`
+
+	cfg := getConfig(spec, false)
+	kazaamOut, _ := getTransformTestWrapper(Coalesce, cfg, jsonInput)
+	areEqual, _ := checkJSONBytesEqual(kazaamOut, []byte(jsonOut))
+
+	if !areEqual {
+		t.Error("Transformed data does not match expectation.")
+		t.Log("Expected:   ", jsonOut)
+		t.Log("Actual:     ", string(kazaamOut))
+		t.FailNow()
+	}
+}
