4bd48c65b2a4a02c400947480d5a649528c2d4f5
Michael Bordner <mbordner@gmail.com>
2019-06-06 00:05:38 -0400

    adding in support to get the length of array values

--- converter/len.go
+++ converter/len.go
@@ -1,6 +1,7 @@
 package converter
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/mbordner/kazaam/transform"
 	"github.com/pkg/errors"
@@ -13,20 +14,28 @@ type Len struct {
 func (c *Len) Convert(jsonData []byte, value []byte, args []byte) (newValue []byte, err error) {
 	newValue = value
 
-	var jsonValue *transform.JSONValue
-	jsonValue, err = c.NewJSONValue(value)
-	if err != nil {
-		return
+	var arrayTest []interface{}
+	err = json.Unmarshal(value, &arrayTest)
+	if err == nil {
+
+		newValue = []byte(fmt.Sprintf("%d", len(arrayTest)))
+
+	} else {
+		var jsonValue *transform.JSONValue
+		jsonValue, err = c.NewJSONValue(value)
+		if err != nil {
+			return
+		}
+
+		if jsonValue.IsString() == false {
+			err = errors.New("value must be string for len converter")
+			return
+		}
+
+		origValue := jsonValue.GetStringValue()
+
+		newValue = []byte(fmt.Sprintf("%d", len(origValue)))
 	}
-
-	if jsonValue.IsString() == false {
-		err = errors.New("value must be string for len converter")
-		return
-	}
-
-	origValue := jsonValue.GetStringValue()
-
-	newValue = []byte(fmt.Sprintf("%d", len(origValue)))
 
 	return
 }
